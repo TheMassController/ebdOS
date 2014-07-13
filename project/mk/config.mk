1f504539097e70bdbe728b3c957765df37fdc568
Jacko Dirks <jacko.dirks@gmail.com>
2014-07-13 12:17:46 +0200

    Started nonrec-make

---
+++ project/mk/config.mk
@@ -0,0 +1,6 @@
+ARMGCCDIR := ~/gcc-arm-none-eabi/
+
+CPP := $(ARMGCCDIR)/bin/arm-none-eabi-g++
+CC := $(ARMGCCDIR)/bin/arm-none-eabi-gcc
+LD := $(ARMGCCDIR)/bin/arm-none-eabi-ld
+OBJCOPY := $(ARMGCCDIR)/bin/arm-none-eabi-objcopy

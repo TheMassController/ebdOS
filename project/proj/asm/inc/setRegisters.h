e33b80e01885d20c5c035ff84dc3d1b902c9ee98
Jacko Dirks <jacko.dirks@gmail.com>
2014-08-16 23:21:01 +0200

    Added setRegisters function, added more testcode

---
+++ project/proj/asm/inc/setRegisters.h
@@ -0,0 +1,8 @@
+//This file has the function protoypes for all assembly functions
+
+//Gets the main stackpointer
+void setMSP(void*);
+//Gets the program stackpointer
+void setPSP(void*);
+void setR7(void*);
+

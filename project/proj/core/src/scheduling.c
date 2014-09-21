ca3646b768216b4dd61ecbfd71f297794548e92d
Jacko Dirks <jacko.dirks@gmail.com>
2014-09-21 15:24:05 +0200

    Setup correct return, untested

--- project/proj/core/src/scheduling.c
+++ project/proj/core/src/scheduling.c
@@ -3,11 +3,20 @@
 #include "getSetRegisters.h"
 #include "regDumpLoad.h"
 #include "stdlib.h"
+#include "asmUtils.h"
+
+//Defines for correct returns
+// See datasheet pp 106
+#define PSPNONFP_RETURN ((void*)0xFFFFFFFD)
+#define MSPNONFP_RETURN ((void*)0xFFFFFFF9)
+#define PSPFP_RETURN    ((void*)0xFFFFFFED)
+#define MSPFP_RETURN    ((void*)0xFFFFFFE9)
 
 // This file handles scheduling, basically
 
 extern struct Process* kernel;
 extern struct Process* currentProcess;
+extern struct Process* firstProcess;
 struct Process* nextProcess = NULL;
 
 void switchFromKernel(void){
@@ -37,7 +46,11 @@ void pendSVHandler(void){
         switchProcesses();
     }
     currentProcess = nextProcess;
-    //TODO correct return
+    if (nextProcess->pid == 0){
+        forceManualReturn(MSPNONFP_RETURN);
+    } else {
+        forceManualReturn(PSPNONFP_RETURN);
+    }
 }
 
 

8433169b961914def21e5939355dd917fb058dec
Jacko <jacko.dirks@gmail.com>
2014-11-18 22:57:46 +0100

    Added more queue functionality

--- project/proj/core/src/queue.c
+++ project/proj/core/src/queue.c
@@ -22,14 +22,46 @@ void deleteQueue(Queue* queue){
     //TODO implement
 }
 
+//ONLY CALL THIS FUNCTION WHEN THE MUTEX IS LOCKED!!!
+void __addToQeue(Queue* queue, QueueItem* item){
+    if (queue->firstItem == NULL){
+        queue->firstItem = item;
+        queue->lastItem = item;
+        item->previousItem = NULL;
+    }else{
+        queue->firstItem->previousItem = item;
+        queue->firstItem = item;
+    }
+}
+
+//ONLY CALL THIS FUNCTION WHEN THE MUTEX IS LOCKED!!!
+QueueItem* __popFromQueue(Queue* queue){
+    if (queue->lastItem == NULL) return NULL;
+    QueueItem* item = queue->lastItem;    
+    if (queue->firstItem == queue->lastItem){
+        queue->firstItem = NULL;
+        queue->lastItem = NULL;
+    } else {
+        queue->lastItem = queue->lastItem->previousItem;
+    }
+    return item;
+}
+
 int pushToQueue(Queue* queue, void* item){
     QueueItem* queueItem = (QueueItem*)malloc(sizeof(QueueItem)); 
     queueItem->data = item;
     if (item == NULL) return 0;
-    lockMutexBlocking(queue->mutex);   
-    queueItem->nextItem = queue->firstItem;
-    queue->firstItem->previousItem = queueItem;
-    queue->firstItem = queueItem;
+    lockMutexBlocking(queue->mutex);
+    __addToQeue(queue, queueItem);
     releaseMutex(queue->mutex);
     return 1;
+}
+
+void* popFromQueue(Queue* queue){
+    lockMutexBlocking(queue->mutex);
+    QueueItem* item = __popFromQueue(queue); 
+    releaseMutex(queue->mutex);
+    void* object = item->data;
+    free(item);
+    return object; 
 }

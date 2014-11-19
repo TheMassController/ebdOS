e08b92e7793ca857c6ab3d2bf518839bb4076ebe
Jacko <jacko.dirks@gmail.com>
2014-11-19 14:06:31 +0100

    Made malloc reentrant, added some commentary

--- project/proj/core/inc/binaryMutex.h
+++ project/proj/core/inc/binaryMutex.h
@@ -1,23 +1,49 @@
 #ifndef BINARYMUTEX_H
 #define BINARYMUTEX_H
+/**
+@file binaryMutex.h
+@author J. Dirks
 
-//Binary mutex functions.
-//A binary mutex is anonymous: everyone can release it.
-//You cannot grab it twice: if you try to grab it a second time, you will need to wait for a release first
+Creation, deletion and usage of binary mutexes
+
+Binary mutex functions.
+A binary mutex is anonymous: everyone can release it.
+You cannot grab it twice: if you try to grab it a second time, you will need to wait for a release first
+*/
+
 
 struct BinaryMutex{
     unsigned lock;
     struct BinaryMutex* nextBinaryMutex;
 };
 
+/** The actual BinaryMutex */
 typedef struct BinaryMutex BinaryMutex;
 
+/**
+Locks the binary mutex, blocks until the mutex is locked.
+
+@param mutex The mutex you want to block.
+*/
 void lockBinaryMutexBlocking(BinaryMutex* mutex);
 
-//Returns 1 if successfull, 0 if not
+/**
+Tries to lock the binary mutex. If unsucsessfull, blocks until the mutex can be locked or timeout, whichever comes first.
+
+@param mutex The mutex you want to block.
+@param msWaitTime Maximum blocking time.
+
+@return 1 if the mutex was sucsessfully locked, else 0.
+*/
 int lockBinaryMutexBlockWait(BinaryMutex* mutex, unsigned msWaitTime);
 
-//Returns 1 if successfull, 0 if not
+/**
+Attemps one time to lock the binary mutex.
+
+@param mutex The mutex you want to lock
+
+@return 1 if the mutex was sucsessfully locked, else 0.
+*/
 int lockBinaryMutexNoBlock(BinaryMutex* mutex);
 
 void releaseBinaryMutex(BinaryMutex* mutex);

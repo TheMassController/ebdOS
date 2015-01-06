3999f654f99d5fb5f4c951aab4eca4b40fce4c6b
Jacko Dirks <jacko.dirks@gmail.com>
2015-01-06 15:27:23 +0100

    Sleep works again, updates to order: SVC can now be called from interrupts

--- project/proj/core/src/fault.c
+++ project/proj/core/src/fault.c
@@ -2,8 +2,15 @@
 
 #include "uartstdio.h"
 #include "asmUtils.h"
+#include "lm4f120h5qr.h"
 
 void faultISRHandler(void){
     UARTprintf("Hard fault\r\nReset the device to continue...\r\n");
+    while(1);
+}
+
+void usageFaultHandler(void){
+    UARTprintf("Usage fault occured\r\n");
+    UARTprintf("Usage fault status register: 0x%x\r\n",NVIC_FAULT_STAT_R);
     while(1);
 }

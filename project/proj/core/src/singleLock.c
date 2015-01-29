c4714b2d64fa5d58067f12ae549a8913013b8e6f
Jacko Dirks <jacko.dirks@gmail.com>
2015-01-29 16:58:10 +0100

    Fixed releasing locks from interrupts

--- project/proj/core/src/singleLock.c
+++ project/proj/core/src/singleLock.c
@@ -3,6 +3,7 @@
 #include "supervisorCall.h"
 #include "sleep.h"
 #include "stdlib.h" 
+#include "asmUtils.h"
 
 extern struct Process* currentProcess;
 
@@ -26,8 +27,8 @@ int __lockObjectNoblock(SingleLockObject* object){
 
 void __lockObjectBlock(SingleLockObject* object){
    while(!__lockSingleLockObject(object)){
-        currentProcess->blockAddress = object;
         currentProcess->state |= STATE_LOCKED;
+        currentProcess->blockAddress = object;
         CALLSUPERVISOR(SVC_objectLock);
     } 
      //TODO maybe implement a way to increase the priority of the process that holds this mutex, if the prio of the current process is higher
@@ -47,10 +48,30 @@ int __lockObjectBlockTimeout(SingleLockObject* object, unsigned msTimeout){
     return __lockObjectNoblock(object);
 }
 
+//Th purpose of this function is to become "interrupt-safe"
+/**
+    Interrupt introduces two new problems:
+    -An interrupt is not a process, but it utializes the blockAddress of a process
+    this might lead to a wrong block address in the process
+    -Interrupts can be nested, and thus the wrong parameter can be passed to the supervisor
+
+    This solution solves both, without the need for a seperate piece of code just for the interrupt
+**/
+void callForRelease(void* oldBlockAddr){
+    CALLSUPERVISOR(SVC_objectRelease);
+    currentProcess->blockAddress = oldBlockAddr;
+}
+
 void __releaseObject(SingleLockObject* object){
     __unlockSingleLockObject(object);
-    currentProcess->blockAddress = object;
-    CALLSUPERVISOR(SVC_objectRelease);
+    if (isInInterrupt()){
+        void* oldBlockAddress = currentProcess->blockAddress;
+        currentProcess->blockAddress = object;
+        callForRelease(oldBlockAddress);
+    } else {
+        currentProcess->blockAddress = object;
+        CALLSUPERVISOR(SVC_objectRelease);
+    }
 }
 
 int __singleLockObjectIsLocked(SingleLockObject* object){

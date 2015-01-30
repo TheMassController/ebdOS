9d23da89e6ef849af75c52c16e68372f98a75134
Jacko Dirks <jacko.dirks@gmail.com>
2015-01-30 16:09:23 +0100

    Changed from C99 to C11

--- project/proj/mk/flags.mk
+++ project/proj/mk/flags.mk
@@ -1,5 +1,5 @@
 LDFLAGS = -T$(LINKER_SCRIPT) --entry ResetISR --gc-sections $(LIBDIR)
-CFLAGS = -mcpu=cortex-m4 -mfloat-abi=softfp -ffunction-sections -fdata-sections -fomit-frame-pointer -std=c99 -Wall -Wextra -Werror -MMD -MP -mthumb $(INC) -DPART_LM4F120H5QR -DTARGET_IS_BLIZZARD_RA1
+CFLAGS = -mcpu=cortex-m4 -mfloat-abi=softfp -ffunction-sections -fdata-sections -fomit-frame-pointer -std=c11 -Wall -Wextra -Werror -MMD -MP -mthumb $(INC) -DPART_LM4F120H5QR -DTARGET_IS_BLIZZARD_RA1
 #-DPART_LM4F120H5QR -c $(INC) -DTARGET_IS_BLIZZARD_RA1 -mfpu=fpv4-sp-d16 -mthumb
 ASMFLAGS = -c -mthumb -mcpu=cortex-m4 -mfloat-abi=softfp -ffunction-sections -fdata-sections
 OBJCOPYFLAGS = 

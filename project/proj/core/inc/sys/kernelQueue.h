6e1d6d893d3c21e65ba3293d3c4553f506521408
Jacko <jacko.dirks@gmail.com>
2015-02-08 22:46:51 +0100

    Some progress on Kernel interaction

--- project/proj/core/inc/sys/kernelQueue.h
+++ project/proj/core/inc/sys/kernelQueue.h
@@ -1,44 +1,13 @@
 #ifndef KERNELQUEUE_H
 #define KERNELQUEUE_h
 
-#include "process.h"
-#include "semaphore.h"
-#include "mutex.h"
-
-enum KQITEMTYPE
-{
-    newprocess,
-    deleteprocess,
-   addsleeper
-};
-
-struct KernelQueueItem{
-    enum KQITEMTYPE itemtype;
-    void* item;  
-    struct KernelQueueItem* nextItem;
-};
-
-struct KernelQueue{
-    struct Semaphore* existingItems;
-    struct Semaphore* readyItems;
-    struct Mutex* listProtectionMutex;
-    struct KernelQueueItem* firstItem;
-};
-
-struct NewProcess{
-    unsigned mPid;
-    unsigned long stacklen;
-    char* name;
-    void (*procFunc)(void*);
-    void* param;
-    char priority;
-};
-
-struct DeleteProcess{
-    struct Process* processToDelete;
-};
+/**
+    RETCODES:
+    0: sucess
+    2: unknown error
+**/
 
 struct KernelQueueItem* __popItem(void);
-void createProcess(unsigned long stacklen, char* name, void (*procFunc)(void*), void* param, char priority);
-void __deleteProcess(struct Process* processToDelete);
+int createProcess(unsigned long stacklen, char* name, void (*procFunc)(void*), void* param, char priority);
+int __deleteProcess(struct Process* processToDelete);
 #endif //KERNELQUEUE_h

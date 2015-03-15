5544534bfc822ffe27ece018669e2efb0bc5b979
Jacko Dirks <jacko.dirks@gmail.com>
2015-03-15 16:59:19 +0100

    Finished switch to Hard FP

--- project/proj/core/src/fault.c
+++ project/proj/core/src/fault.c
@@ -1,30 +1,39 @@
 //This is where a fault lands
-#define DEBUG //For now
+#ifndef DEBUG
+    #define DEBUG //For now
+#endif //DEBUG
 #ifdef DEBUG
 #include "uartstdio.h"
 #include "lm4f120h5qr.h"
+#include "asmUtils.h"
 #endif //DEBUG
 
 void faultISRHandler(void){
 #ifdef DEBUG
     UARTprintf("Hard fault\r\nReset the device to continue...\r\n");
 #endif //DEBUG
-    while(1);
+    while(1) waitForInterrupt();
 }
 
 void usageFaultHandler(void){
 #ifdef DEBUG
     UARTprintf("Usage fault occured\r\n");
-    UARTprintf("Usage fault status register: 0x%x\r\n",NVIC_FAULT_STAT_R);
+    UARTprintf("UFAULTSTAT info:\r\n");
+    UARTprintf("\tDivide-by-Zero Usage Fault: %d\r\n", (NVIC_FAULT_STAT_R & NVIC_FAULT_STAT_DIV0) ? 1 : 0);
+    UARTprintf("\tUnaligned Access Usage Fault: %d\r\n", (NVIC_FAULT_STAT_R & NVIC_FAULT_STAT_UNALIGN) ? 1 : 0);
+    UARTprintf("\tNo Coprocessor Usage Fault: %d\r\n", (NVIC_FAULT_STAT_R & NVIC_FAULT_STAT_NOCP) ? 1 : 0);
+    UARTprintf("\tInvalid PC Load Usage Fault: %d\r\n", (NVIC_FAULT_STAT_R & NVIC_FAULT_STAT_INVPC) ? 1 : 0);
+    UARTprintf("\tInvalid State Usage Fault: %d\r\n", (NVIC_FAULT_STAT_R & NVIC_FAULT_STAT_INVSTAT) ? 1 : 0);
+    UARTprintf("\tUndefined Instruction Usage Fault: %d\r\n", (NVIC_FAULT_STAT_R & NVIC_FAULT_STAT_UNDEF) ? 1 : 0);
 #endif //DEBUG
-    while(1);
+    while(1) waitForInterrupt();
 }
 
 void mpuFaultHandler(void){
 #ifdef DEBUG
     UARTprintf("MPU fault occured\r\n");
 #endif //DEBUG
-    while(1);
+    while(1) waitForInterrupt();
 }
 
 void busFaultHandler(void){
@@ -41,5 +50,5 @@ void busFaultHandler(void){
     
     UARTprintf("\r\nBus Fault Address value: 0x%x\r\n", NVIC_FAULT_ADDR_R);
 #endif //DEBUG
-    while(1);
+    while(1) waitForInterrupt();
 }

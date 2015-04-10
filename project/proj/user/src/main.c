871c4dbd9f9e1f7c0256ed3c05dad4fb2d0cf1c8
Jacko Dirks <jacko.dirks@gmail.com>
2015-04-10 16:11:14 +0200

    Fixed a whole bunch of stuff, MPU is still not flawless

--- project/proj/user/src/main.c
+++ project/proj/user/src/main.c
@@ -13,9 +13,9 @@ int main(void){
     //rest semaphores
     //Test the combination of sleep and mutex (blockandwait)
     //return testScheduler();
-    //if(__createNewProcess(0, 512, "testFloat", testFloatMain, NULL, 5) == 2){
-    //    UARTprintf("FAILURE\r\n");
-    //}
+    if(__createNewProcess(0, 512, "testFloat", testFloatMain, NULL, 5) == 2){
+        UARTprintf("FAILURE\r\n");
+    }
     UARTprintf("%u\n", sizeof(unsigned));
     ledsDanceMain();
 }

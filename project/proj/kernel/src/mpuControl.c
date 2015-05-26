3478fe52ec1c1db34752018fcdf55fef709c7eef
Jacko <jacko.dirks@gmail.com>
2015-05-26 14:43:36 +0200

    More cleanup

--- project/proj/kernel/src/mpuControl.c
+++ project/proj/kernel/src/mpuControl.c
@@ -1,46 +1,29 @@
 //Responsible for the MPU control
-#include "hw_types.h" //Contains the types
-#include "rom_map.h" //Call functions directly from the ROM if available
-#include "rom.h" //Declare ROM addresses for rom funcs
+//System headers
+#include <hw_types.h> //Contains the types
+#include <rom_map.h> //Call functions directly from the ROM if available
+#include <rom.h> //Declare ROM addresses for rom funcs
 #include <mpu.h>
 #include <hw_memmap.h>
 #include <hw_nvic.h>
 #include <hw_ints.h>
-
-#include "uartstdio.h"
+#include <uartstdio.h>
+//User headers
 #include "utilFuncs.h"
 
-//Flash addresses
+//All of these are determined by the linker, see linker_script.ld
 extern unsigned long _flash_start;
 extern unsigned long _flash_end;
-
-//SRAM addresses
 extern unsigned long _sram_start;
 extern unsigned long _sram_end;
-
-//kernel code start
 extern unsigned long _kernel_text;
-//Core code start
 extern unsigned long _core_text;
-//Flash end
 extern unsigned long _flash_text_data_end_aligned;
 
-//Datsheet pp 124
-#define PRIV_NO_USR_NO 0
-//      PRIV_NO_USR_RO      //These do not exist
-//      PRIV_NO_USR_RW      //These do not exist
-#define PRIV_RO_USR_NO 5    //101
-#define PRIV_RO_USR_RO 7    //111
-//      PRIV_RO_USR_RW      //These do not exist
-#define PRIV_RW_USR_NO 1    //001
-#define PRIV_RW_USR_RO 2    //010
-#define PRIV_RW_USR_RW 3    //011
-
-
-
-unsigned isPowerOfTwo(unsigned regionLen){
-    if ((regionLen & (regionLen - 1)) == 0) {
-        return regionLen;
+//Returns 0 if given param is not a power of 2
+unsigned isPowerOfTwo(unsigned size){
+    if ((size & (size - 1)) == 0) {
+        return size;
     } else {
         return 0;
     }
@@ -48,6 +31,7 @@ unsigned isPowerOfTwo(unsigned regionLen){
 
 //Returns 1 if everything went ok
 unsigned setMPURegion(unsigned char regionNumber, unsigned baseAddr, unsigned size, unsigned settings){
+    //Test the params for sanity. The ROM function does not return, so we have to catch errors here.
     if (!isPowerOfTwo(size) || (unsigned)baseAddr % size || size == 0 || size < 32 || regionNumber > 7 ){
         return 0;
     }
@@ -70,30 +54,32 @@ unsigned setMPURegion(unsigned char regionNumber, unsigned baseAddr, unsigned si
 }
 
 void initializeMPU(void){
-    //Region 0 protects the entire SRAM, making it RO, RO
+    //The sizes
     const unsigned FLASHSIZE = (unsigned)&_flash_end - (unsigned)&_flash_start;
     const unsigned KERNELFLASHSIZE = (unsigned)&_core_text - (unsigned)&_kernel_text;
     const unsigned PUBLICFLASHSIZE = (unsigned)&_flash_text_data_end_aligned - (unsigned)&_core_text;
     const unsigned SRAMSIZE = (unsigned)&_sram_end - (unsigned)&_sram_start;
-
+    //The baseadresses
     const unsigned FLASHSTART = (unsigned long)&_flash_start;
     const unsigned KERNELFLASHSTART = (unsigned long)&_kernel_text;
     const unsigned PUBLICFLASHSTART = (unsigned long)&_core_text;
     const unsigned SRAMSTART = (unsigned long)&_sram_start;
-
-
+    //Global flash region: the fallback region
     if (!setMPURegion(0, FLASHSTART, FLASHSIZE, MPU_RGN_PERM_NOEXEC | MPU_RGN_PERM_PRV_RW_USR_NO | MPU_RGN_ENABLE)){
         UARTprintf("Setting up the default flash region failed\n");
         generateCrash();
     }
+    //The kernelflash region: only readable for privilege
     if (!setMPURegion(1, KERNELFLASHSTART, KERNELFLASHSIZE, MPU_RGN_PERM_EXEC | MPU_RGN_PERM_PRV_RO_USR_NO | MPU_RGN_ENABLE)){
         UARTprintf("Setting up the kernel flash region failed\n");
         generateCrash();
     }
+    //The public region, readable for everyone
     if (!setMPURegion(2, PUBLICFLASHSTART, PUBLICFLASHSIZE, MPU_RGN_PERM_EXEC | MPU_RGN_PERM_PRV_RO_USR_RO | MPU_RGN_ENABLE)){
         UARTprintf("Setting up the public flash region failed\n");
         generateCrash();
     }
+    //The global SRAM region: the fallback region
     if (!setMPURegion(3, SRAMSTART, SRAMSIZE,  MPU_RGN_PERM_NOEXEC | MPU_RGN_PERM_PRV_RW_USR_RW | MPU_RGN_ENABLE)){
         UARTprintf("Setting up the global SRAM region failed\n");
         generateCrash();

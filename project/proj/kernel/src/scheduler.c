6a07f8a730587267c6ae50d29287a7b202faa0d6
Jacko Dirks <jacko.dirks@gmail.com>
2015-06-25 10:16:00 +0200

    Some more progress on scheduler reimplementation

---
+++ project/proj/kernel/src/scheduler.c
@@ -0,0 +1,9 @@
+//External headers
+#include <stdlib.h>
+//Internal headers
+#include "scheduler.h"  // Contain the declarations for all functions defined here.
+#include "process.h"    // The struct Process, a lot of defines related to processes.
+//TODO make processesReady static.
+struct Process* processesReady = NULL;
+
+

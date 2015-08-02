7bc58959a7e0b13efe28ed2d1efa389966bf0780
Jacko <jacko.dirks@gmail.com>
2015-08-02 15:52:35 +0200

    Made this world a nicer place

--- project/proj/kernel/inc/supervisorCall.h
+++ project/proj/kernel/inc/supervisorCall.h
@@ -6,19 +6,19 @@
 #define STR(x) STR_HELPER(x)
 
 // The codes
-#define SVC_yield 0
-#define SVC_multiObjectIncrease 4
-#define SVC_multiObjectDecrease 5
-#define SVC_multiObjectWaitForIncrease 6
-#define SVC_multiObjectWaitForDecrease 7
-#define SVC_multiObjectWaitForIncreaseAndSleep 8
-#define SVC_multiObjectWaitForDecreaseAndSleep 9
-#define SVC_sleep 11
-#define SVC_wakeup 12
-#define SVC_wakeupCurrent 13
-#define SVC_processAdd 14
+#define SVC_yield                               0
+#define SVC_multiObjectIncrease                 1
+#define SVC_multiObjectDecrease                 2
+#define SVC_multiObjectWaitForIncrease          3
+#define SVC_multiObjectWaitForDecrease          4
+#define SVC_multiObjectWaitForIncreaseAndSleep  5
+#define SVC_multiObjectWaitForDecreaseAndSleep  6
+#define SVC_sleep                               7
+#define SVC_wakeup                              8
+#define SVC_wakeupCurrent                       9
+#define SVC_processAdd                          10
 #ifdef DEBUG
-#define SVC_test 255
+#define SVC_test                                255
 #endif // DEBUG
 
 // Contains the macro to create the supervisor call

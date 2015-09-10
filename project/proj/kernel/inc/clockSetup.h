aad234a84c1142aadc5b2260c6bacca48aad9ca0
Jacko Dirks <jacko.dirks@gmail.com>
2015-09-10 17:01:42 +0200

    Patched up clock settings

--- project/proj/kernel/inc/clockSetup.h
+++ project/proj/kernel/inc/clockSetup.h
@@ -14,26 +14,29 @@
 // Base speed after PLL is 200 MHz. 80 is the max the CPU can handle
 
 #define PC_BAUDRATE 115200
+#define EDB_OS_CLOCK_80MHZ
+
+// The next table defines the possible clock speeds
+const unsigned CLOCKSPEEDS_SYSCTL[] = {
+    (SYSCTL_SYSDIV_2_5| SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ),  // 80 Mhz
+    (SYSCTL_SYSDIV_5| SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ),    // 40 Mhz
+    (SYSCTL_USE_OSC | SYSCTL_OSC_MAIN| SYSCTL_XTAL_16MHZ),                  // 16 Mhz
+    (SYSCTL_SYSDIV_20 | SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ)   // 10 Mhz
+};
 
 // SYSCTL_SETUP is PLL setup
 // BASE_CLOCK_SPEED is global base clock speed after the PLL is set
 // SLEEP_CLOCK_INCREASE_CYCLES the sleep clock increases every so many cycles of the main clock, is always equal to 1 us
-#ifdef EBD_OS_CLOCK_40MHZ
-    #define EBD_SYSCTL_SETUP (SYSCTL_SYSDIV_5| SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ)
-    #define EBD_BASE_CLOCK_SPEED 40000000
-    #define EBD_SLEEP_CLOCK_INCREASE_CYCLES 40
-#elif EBD_OS_CLOCK_16MHZ
-    #define EBD_SYSCTL_SETUP (SYSCTL_USE_OSC | SYSCTL_OSC_MAIN| SYSCTL_XTAL_16MHZ)
-    #define EBD_BASE_CLOCK_SPEED 16000000
-    #define EBD_SLEEP_CLOCK_INCREASE_CYCLES 16
-#elif EBD_OS_CLOCK_10MHZ
-    #define EBD_SYSCTL_SETUP (SYSCTL_SYSDIV_20| SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ)
-    #define EBD_BASE_CLOCK_SPEED 10000000
-    #define EBD_SLEEP_CLOCK_INCREASE_CYCLES 10
-#else // Default is 80 Mhz
-    #define EBD_SYSCTL_SETUP (SYSCTL_SYSDIV_2_5| SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ)
-    #define EBD_BASE_CLOCK_SPEED 80000000
-    #define EBD_SLEEP_CLOCK_INCREASE_CYCLES 80
+#ifdef  EDB_OS_CLOCK_80MHZ
+    #define EDB_SYSCTL_ENTRY 0
+#elif   EBD_OS_CLOCK_40MHZ
+    #define EDB_SYSCTL_ENTRY 1
+#elif   EBD_OS_CLOCK_16MHZ
+    #define EDB_SYSCTL_ENTRY 2
+#elif   EBD_OS_CLOCK_10MHZ
+    #define EDB_SYSCTL_ENTRY 3
+#else
+    #error "No clockspeed was defined!"
 #endif
 
 

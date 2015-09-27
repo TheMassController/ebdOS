281463988ca88c4bc56f2fd3937bd31d8c344f6a
Jacko Dirks <jacko.dirks@gmail.com>
2015-09-27 21:03:17 +0200

    Added code to sleepModule

--- project/proj/kernel/src/kernSleepModule.c
+++ project/proj/kernel/src/kernSleepModule.c
@@ -1,5 +1,6 @@
 #include <stdint.h>                     // uint64_t
 #include <stdlib.h>                     // NULL
+#include <string.h>                     // strerror
 
 #include "supervisorCall.h"             // Contains the macro's necessary for making a supervisor call
 #include "process.h"                    // Contains information about the struct Process
@@ -8,7 +9,7 @@
 #include "coreUtils.h"                  // For testing if inside SVC interrupt
 #include "spinlock.h"                   // For the spinlock
 #ifdef DEBUG
-#include <uartstdio.h>
+#include <uartstdio.h>                  // Printfs etc.
 #include "kernUtils.h"                  // Generate crash
 #endif //DEBUG
 
@@ -22,18 +23,44 @@ static void generateSleepInterrupt(void){
 
 static struct Process* internalAddSleeper(struct Process* proc){
     struct SleepingProcessStruct* sleepStruct = &(proc->sleepObj);
+    int errcode;
     if (sleepStruct->overflows == 0 && sleepStruct->sleepUntil <= getSystemclockValue()) return proc;
 #ifdef DEBUG
-    if (!tryLockSpinlock(&spinlock)){
-        UARTprintf("internalAddSleeper: getting the spinlock failed!\n");
+    if (!(errcode = tryLockSpinlock(&spinlock))){
+        UARTprintf("internalAddSleeper: getting the spinlock failed! returncode: %d, (POSIX? %s)\n", errorcode, strerror(errorcode));
         generateCrash();
     }
 #else
-    while(!tryLockSpinlock(&spinlock));
+    if (!(errcode = lockSpinlock(&spinlock))){
+        UARTprintf("Fatal error in kernel: %s\n", strerror(errorcode));
+    }
 #endif //DEBUG
+    // Sort the process into the sleeplist
+    if (sleepList = NULL){
+        sleepList = proc;
+        proc->nextProcess = NULL;
+    } else {
+        // TODO fix
+        struct Process* current = sleepList;
+        // First part of search: sort into the correct amount of overflows
+         for (; current->nextProcess != NULL; current = current->nextProcess){
+             if ((current->sleepObj).overflows >= (proc->sleepObj).overflows)
+                 break;
+         }
+         // Then sort into the correct wakeup moment. Higher goes first
+         for (; current->nextProcess != NULL; current = current->nextProcess){
+             if ((current->sleepObj).sleepUntil > (proc->sleepObj).sleepUntil)
+                 break;
+         }
+         if (current == sleepList){
+             sleepList = proc;
+             proc->nextProcess = current;
+         } else {
 
+         }
+        
+    }
     unlockSpinlock(&spinlock);
-:x
     return NULL;
 }
 
@@ -41,8 +68,16 @@ struct Process* addSleeper(struct Process* proc, struct SleepRequest* sleepReque
     //Usually called from kernel
     struct SleepingProcessStruct* sleepStruct = &(proc->sleepObj);
     sleepStruct->nextPtr = NULL;
-    uint64_t wakeupMoment = sleepRequest->uSec + sleepRequest->mSec * 1000 + sleepRequest->sec * 1000000 + sleepRequest->refTime;
-    sleepStruct->overflows = wakeupMoment / EBD_SYSCLOCKMAXVAL;
-    sleepStruct->sleepUntil = wakeupMoment % EBD_SYSCLOCKMAXVAL;
+    /* The systemClock runs from max to min. So if we want to sleep for x time, we want to wait until reftime - x
+     * First of all: we need the total amount of us we are going to sleep. */
+    uint64_t wakeupMoment = sleepRequest->uSec + sleepRequest->mSec * 1000 + sleepRequest->sec * 1000000;
+    /* So now we need to find the overflow count. We basically remove the max count of the sysclock from the wakeupMoment and add 1 to the overflow until wakeupMoment <= sleepRequest->reftime
+     */
+    sleepStruct->overflows = 0;
+    for(; wakeupMoment > sleepRequest->refTime; wakeupMoment -= EBD_SYSCLOCKMAXVAL) {
+        sleepStruct->overflows += 1;
+    }
+    sleepStruct->sleepUntil = sleepRequest->refTime - wakeupMoment;
+    // This function does the actual addition.
     return internalAddSleeper(proc);
 }

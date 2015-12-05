24cfe245cb9827cb3b66ba150a2ac4e47914d93a
Jacko Dirks <jacko.dirks@gmail.com>
2015-12-05 16:39:30 +0100

    Added the anonymous spinlock

---
+++ project/proj/core/src/anonymousSpinlock.c
@@ -0,0 +1,56 @@
+// User headers
+#include "anonymousSpinlock.h"                   // Implies include process.h. Used to define the AnonymousSpinlock struct
+#include "supervisorCall.h"             // Needed for supervisor calls
+#include "atomicIntegerOperations.h"    // Needed for threadsafe integer operations
+// System headers
+#include <errno.h>              // Contains the error values
+#include <stdlib.h>             // Contains the def of NULL
+
+int initAnonymousSpinlock(struct AnonymousSpinlock* anonymousSpinlock){
+    if (anonymousSpinlock != NULL){
+        anonymousSpinlock->value = 0;
+        return 0;
+    }
+    return EINVAL;
+}
+
+int destroyAnonymousSpinlock(struct AnonymousSpinlock* anonymousSpinlock){
+    if (anonymousSpinlock != NULL){
+        if (anonymousSpinlock->value == 1){
+            return EBUSY;
+        } else {
+            return 0;
+        }
+    }
+    return EINVAL;
+}
+
+int lockAnonymousSpinlock(struct AnonymousSpinlock* anonymousSpinlock){
+    if (anonymousSpinlock != NULL){
+        while(atomicIncreaseIntWithMax(&anonymousSpinlock->value, 1) == -1){
+            CALLSUPERVISOR(SVC_yield);
+        }
+        return 0;
+    }
+    return EINVAL;
+}
+
+int tryLockAnonymousSpinlock(struct AnonymousSpinlock* anonymousSpinlock){
+    if (anonymousSpinlock != NULL){
+        if (atomicIncreaseIntWithMax(&anonymousSpinlock->value, 1) == 1){
+            return 0;
+        } else {
+            return EBUSY;
+        }
+    }
+    return EINVAL;
+}
+
+int unlockAnonymousSpinlock(struct AnonymousSpinlock* anonymousSpinlock){
+    if (anonymousSpinlock != NULL){
+        // We do not care if this fails or not
+        atomicDecreaseIntWithMax(&anonymousSpinlock->value);
+        return 0;
+    }
+    return EINVAL;
+}

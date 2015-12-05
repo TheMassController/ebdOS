b476ea5a7891def26141545893415205302adc07
Jacko Dirks <jacko.dirks@gmail.com>
2015-12-05 16:40:17 +0100

    Expanded the KernInteractBuffer with more threadsafety

--- project/proj/kernel/src/kernInteractBuffer.c
+++ project/proj/kernel/src/kernInteractBuffer.c
@@ -1,5 +1,5 @@
 #include "kernInteractBuffer.h"
-#include "spinlock.h"
+#include "anonymousSpinlock.h"
 #include "atomicIntegerOperations.h"
 #include "supervisorCall.h"
 
@@ -9,15 +9,16 @@
 //
 // The fact that all of the next fields are initialized to 0 is exploited like nobody's buisiness.
 static enum KernBufferMessageCodes messageBuf[KERNMESSAGEBUFFERSIZE];
-static struct Process* processBuf[MAXTOTALPROCESSES];
+static struct AnonymousSpinlock messageBufLock[KERNMESSAGEBUFFERSIZE];
+//static struct Process* processBuf[MAXTOTALPROCESSES];
 // The pointers for the messagebuffer.
 static int messageReadPos;
 static int messageWritePos;
 // The pointers for the process buffer.
-static int processReadPos;
-static int processWritePos;
+//static int processReadPos;
+//static int processWritePos;
 
-static int enoughSpaceInBuffer(int read, int write, const int bufLen){
+static int enoughSpaceInBuffer(int read, int write, const int buflen){
     if (read == 0 && write == buflen-1) return 0;
     if (write == read-1) return 0;
     return 1;
@@ -26,25 +27,23 @@ static int enoughSpaceInBuffer(int read, int write, const int bufLen){
 int passMessageToKernel(const enum KernBufferMessageCodes code){
     while(1){
         if (!enoughSpaceInBuffer(messageReadPos, messageWritePos, KERNMESSAGEBUFFERSIZE)) return ENOMEM; 
-        int writepos = atomicIncreaseInt(&messageWritePos);
-        if (writepos >= KERNMESSAGEBUFFERSIZE){
+        int writepos = atomicIncreaseIntWithMax(&messageWritePos, KERNMESSAGEBUFFERSIZE-1);
+        if (writepos == -1){
             if (!atomicSetInt(&messageWritePos, 0, writepos)) continue;
             writepos = 0;
         }
+        lockAnonymousSpinlock(&messageBufLock[writepos]);
         messageBuf[writepos] = code;
+        unlockAnonymousSpinlock(&messageBufLock[writepos]);
         return 0;
     }
 }
-
+// TODO make it so that multiple threads can read from this without any issue
 enum KernBufferMessageCodes kernelBufferGetCode(void){
     if (messageReadPos == messageWritePos) return noMessageAvailable;
-    enum KernBufferMessageCodes message = noMessageAvailable;
-    // This loop is because the kernel might have start reading while the other process has not written yet. Wait for the write!
-    while(message == noMessageAvailable){
-        message = messageBuf[messageReadPos];
-        CALLSUPERVISOR(SVC_yield);
-    }
-    messageBuf[messageReadPos] = noMessageAvailable;
+    lockAnonymousSpinlock(&messageBufLock[messageReadPos]);
+    enum KernBufferMessageCodes message = messageBuf[messageReadPos];
+    unlockAnonymousSpinlock(&messageBufLock[messageReadPos]);
     int readPos = atomicIncreaseIntWithMax(&messageReadPos, KERNMESSAGEBUFFERSIZE-1);
     if (readPos == -1) atomicSetInt(&messageReadPos, 0, KERNMESSAGEBUFFERSIZE-1);
     return message;

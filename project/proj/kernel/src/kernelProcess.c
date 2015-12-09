0042e8f017b2c987df7edf57f177d8cb35bb98a1
Jacko Dirks <jacko.dirks@gmail.com>
2015-12-09 19:24:02 +0100

    Implemented a buffer system for processes between SVC and kernel

--- project/proj/kernel/src/kernelProcess.c
+++ project/proj/kernel/src/kernelProcess.c
@@ -1,23 +1,27 @@
 //The functions of the actual kernel: this is the kernel process
+#include <stdlib.h>
+
 #include "process.h"
 #include "uartstdio.h"
 #include "sysCalls.h"
 #include "supervisorCall.h"
+#include "kernMaintenanceQueue.h"
 
-struct Process* kernReturnList;
-
-void kernelMain(volatile const struct Process** kernMaintenancePtr){
+void kernelMain(void){
     while(1){
-        struct Process* kernMaintenanceProc = (struct Process*)*kernMaintenancePtr;
-        struct ProcessContext* context = kernMaintenanceProc->context;
-        switch(context->comVal){
-            case SYSCALL_getpid:
-                context->retVal = kernMaintenanceProc->pid;
-                kernReturnList = kernMaintenanceProc;
-                break;
-            default:
-                UARTprintf("Unknown code for kernel service: %d", context->comVal);
-                break;
+        struct Process* kernMaintenanceProc = kernelBufferGetProcess();
+        while(kernMaintenanceProc != NULL){
+            struct ProcessContext* context = kernMaintenanceProc->context;
+            switch(context->comVal){
+                case SYSCALL_getpid:
+                    context->retVal = kernMaintenanceProc->pid;
+                    kernRetQueuePush(kernMaintenanceProc);
+                    break;
+                default:
+                    UARTprintf("Unknown code for kernel service: %d, process name: %s, pid: %d\n", context->comVal, kernMaintenanceProc->name, kernMaintenanceProc->pid);
+                    break;
+            }
+            kernMaintenanceProc = kernelBufferGetProcess();
         }
         CALLSUPERVISOR(SVC_serviced);
     }

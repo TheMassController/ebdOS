73e495411d4e4d52c627d9c01ca0a4faa718d2e3
Jacko Dirks <jacko.dirks@gmail.com>
2016-01-03 14:25:09 +0100

    Made debugoutput for kernel events more verbose

--- project/proj/kernel/src/systemClockManagement.c
+++ project/proj/kernel/src/systemClockManagement.c
@@ -6,14 +6,37 @@
 #include "systemClockManagement.h"
 #include "kernEventNotifier.h"
 
+#ifdef DEBUG
+    #include <string.h>     // strerror
+
+    #include "kernUtils.h"  // GenerateCrash
+    #include "uartstdio.h"  // UARTprintf
+#endif //DEBUG
+
 void systemTimerInterrupt(void){
     ROM_TimerIntClear(WTIMER0_BASE,  TIMER_CAPA_MATCH|TIMER_CAPA_EVENT|TIMER_TIMA_TIMEOUT);
+#ifdef DEBUG
+    int code = passMessageToKernel(sysTimerOverflow);
+    if (code != 0){
+        UARTprintf("Failed to pass sysTimerOverflow to kernel. Code: %d (%s)\n", code, strerror(code));
+        generateCrash();
+    }
+#else
     passMessageToKernel(sysTimerOverflow);
+#endif //DEBUG
 }
 
 void sleepTimerInterrupt(void){
     ROM_TimerIntClear(WTIMER0_BASE,  TIMER_TIMB_MATCH);
+#ifdef DEBUG
+    int code = passMessageToKernel(sleepTimerExpired);
+    if (code != 0){
+        UARTprintf("Failed to pass sleepTimerExpired to kernel. Code: %d (%s)\n", code, strerror(code));
+        generateCrash();
+    }
+#else
     passMessageToKernel(sleepTimerExpired);
+#endif //DEBUG
 }
 
 unsigned getSystemClockValue(void){

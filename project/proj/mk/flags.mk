5b4e9c39a55949b8bc8dbdc1df0624b072347257
Jacko Dirks <jacko.dirks@gmail.com>
2016-01-03 14:53:34 +0100

    Cleanup of build: moved varaibles, g3->ggdb3, c11->gnu11. Removed old Makefile

--- project/proj/mk/flags.mk
+++ project/proj/mk/flags.mk
@@ -1,5 +1,9 @@
 LDFLAGS = -T$(LINKER_SCRIPT) -nostdlib --entry ResetISR --gc-sections -u _sbrk -u .isr_vector $(LIBDIR)
-CFLAGS = -mthumb -mcpu=cortex-m4 -mtune=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -ffunction-sections -fdata-sections -fomit-frame-pointer -std=c11 -Wall -Wextra -Werror -MMD -MP $(INC) -DPART_LM4F120H5QR -DTARGET_IS_BLIZZARD_RA2
+CFLAGS = -mthumb -mcpu=cortex-m4 -mtune=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -ffunction-sections -fdata-sections -fomit-frame-pointer -std=gnu11 -Wall -Wextra -Werror -MMD -MP $(INC) -DPART_LM4F120H5QR -DTARGET_IS_BLIZZARD_RA2
 ASMFLAGS = -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16
 OBJCPYFLAGS = -O binary
 ARFLAGS = rcs
+ALLBUILDCFLAGS = -Os
+ALLBUILDLDFLAGS = 
+DEBUGBUILDCFLAGS = -DDEBUG -ggdb3 -Og
+DEBUGGUILDLDFLAGS = 

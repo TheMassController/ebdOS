8127235a461af0335cf7d965e242cea427abc422
Jacko Dirks <jacko.dirks@gmail.com>
2016-01-21 17:47:35 +0100

    Added -Wfatal-errors -Wdouble-promotion warning flags

--- project/proj/mk/flags.mk
+++ project/proj/mk/flags.mk
@@ -2,7 +2,7 @@ LDFLAGS = -T$(LINKER_SCRIPT) -nostdlib --entry ResetISR --gc-sections -u _sbrk -
 MACHINEDEPFLAGS = -mthumb -mcpu=cortex-m4 -mtune=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16
 CODEGENFLAGS = -fsingle-precision-constant -ffunction-sections -fdata-sections -fomit-frame-pointer
 CLANGFLAGS = -std=gnu11
-WARNINGFLAGS = -Wall -Wextra -Wshadow -Werror
+WARNINGFLAGS = -Wall -Wextra -Wshadow -Wfatal-errors -Wdouble-promotion -Werror
 MAKESUPPORTFLAGS = -MMD -MP
 DEFINEFLAGS = -DPART_LM4F120H5QR -DTARGET_IS_BLIZZARD_RA2
 CFLAGS = $(MACHINEDEPFLAGS) $(CODEGENFLAGS) $(CLANGFLAGS) $(WARNINGFLAGS) $(MAKESUPPORTFLAGS) $(INC) $(DEFINEFLAGS)

1e770069682777cf9af3090d391b403fc408aba2
Jacko Dirks <jacko.dirks@gmail.com>
2016-01-21 17:40:15 +0100

    Categorized compiler flags, added -Wshadow

--- project/proj/mk/flags.mk
+++ project/proj/mk/flags.mk
@@ -1,9 +1,15 @@
 LDFLAGS = -T$(LINKER_SCRIPT) -nostdlib --entry ResetISR --gc-sections -u _sbrk -u .isr_vector $(LIBDIR)
-CFLAGS = -mthumb -mcpu=cortex-m4 -mtune=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -ffunction-sections -fdata-sections -fomit-frame-pointer -std=gnu11 -Wall -Wextra -Werror -MMD -MP $(INC) -DPART_LM4F120H5QR -DTARGET_IS_BLIZZARD_RA2
+MACHINEDEPFLAGS = -mthumb -mcpu=cortex-m4 -mtune=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16
+CODEGENFLAGS = -fsingle-precision-constant -ffunction-sections -fdata-sections -fomit-frame-pointer
+CLANGFLAGS = -std=gnu11
+WARNINGFLAGS = -Wall -Wextra -Wshadow -Werror
+MAKESUPPORTFLAGS = -MMD -MP
+DEFINEFLAGS = -DPART_LM4F120H5QR -DTARGET_IS_BLIZZARD_RA2
+CFLAGS = $(MACHINEDEPFLAGS) $(CODEGENFLAGS) $(CLANGFLAGS) $(WARNINGFLAGS) $(MAKESUPPORTFLAGS) $(INC) $(DEFINEFLAGS)
 ASMFLAGS = -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16
 OBJCPYFLAGS = -O binary
 ARFLAGS = rcs
 ALLBUILDCFLAGS = -Os
-ALLBUILDLDFLAGS = 
+ALLBUILDLDFLAGS =
 DEBUGBUILDCFLAGS = -DDEBUG -ggdb3 -Og
-DEBUGGUILDLDFLAGS = 
+DEBUGGUILDLDFLAGS =

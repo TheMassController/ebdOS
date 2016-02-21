9c359fec0d77ddac1b4cf5ade1640b189199741d
Jacko Dirks <jacko.dirks@gmail.com>
2016-02-21 22:29:21 +0100

    Finished non-sleep part of managed lock

--- project/proj/kernel/src/kernManagedLockModule.c
+++ project/proj/kernel/src/kernManagedLockModule.c
@@ -18,6 +18,27 @@ struct SleepQueueElement {
 static struct ManagedLock lockPool[MANAGEDLOCKCOUNT];
 //static struct SleepQueueElement sleepQueuePool[MAXTOTALPROCESSES];
 
+static void addToLockList(struct ManagedLock* lock, struct Process* proc){
+    proc->nextProcess = NULL;
+    proc->state = STATE_WAIT;
+    if (lock->waitinglist == NULL){
+        lock->waitinglist = proc;
+    } else {
+        struct Process* it = lock->waitinglist;
+        while(it->nextProcess != NULL) it = it->nextProcess;
+        it->nextProcess = proc;
+    }
+}
+
+static void removeFromLockList(struct ManagedLock* lock, struct Process* proc){
+    struct Process* it = lock->waitinglist;
+    for (;it->nextProcess != NULL && it != proc;it = it->nextProcess);
+    if (it == proc){
+        lock->waitinglist = it->nextProcess;
+        it->nextProcess = NULL;
+    }
+}
+
 int allocateManagedLock(size_t* lockId, uintptr_t owner){
     size_t iterator;
     for (iterator = 0; iterator < MANAGEDLOCKCOUNT && lockPool[iterator].taken; ++iterator);
@@ -34,5 +55,17 @@ int freeManagedLock(size_t lockId){
     if (lockId >= MANAGEDLOCKCOUNT || !lockPool[lockId].taken) return EINVAL;
     if (lockPool[lockId].waitinglist != NULL) return EDEADLK;
     lockPool[lockId].taken = 0;
+    return 0;
+}
+
+int waitForManagedLock(size_t lockId, struct Process* proc){
+    if (lockId >= MANAGEDLOCKCOUNT || !lockPool[lockId].taken) return EINVAL;
+    addToLockList(&lockPool[lockId], proc);
+    return 0;
+}
+
+int releaseManagedLock(size_t lockId, struct Process* procReady){
+    if (lockId >= MANAGEDLOCKCOUNT || !lockPool[lockId].taken) return EINVAL;
+    removeFromLockList(&lockPool[lockId], procReady);
     return 0;
 }

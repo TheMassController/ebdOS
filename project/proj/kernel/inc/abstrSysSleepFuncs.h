4cd2225be0d01407591f93e9a9ee2b67f87bfce7
Jacko Dirks <jacko.dirks@gmail.com>
2016-04-14 16:58:15 +0200

    Adds documentation to translateSleepRequest

--- project/proj/kernel/inc/abstrSysSleepFuncs.h
+++ project/proj/kernel/inc/abstrSysSleepFuncs.h
@@ -8,7 +8,11 @@
  */
 #include "process.h"
 #include "sleep.h"
-
+/**
+ * @brief Translates a sleepRequest struct to a number of overflows and an additional amount of time, to be written down in the process itself
+ * @param proc The target proc
+ * @param sleepReq The struct
+ */
 void translateSleepRequest(struct Process* proc, const struct SleepRequest* sleepReq);
 
 #endif //ABSTRSYSSLEEPFUNCS_H

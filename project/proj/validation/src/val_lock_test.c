9a8a7bf13877b822320692a329715931175609c5
Jacko Dirks <jacko.dirks@gmail.com>
2016-04-20 00:52:15 +0200

    Adds test for futex wait/timeout combo

--- project/proj/validation/src/val_lock_test.c
+++ project/proj/validation/src/val_lock_test.c
@@ -1,4 +1,5 @@
 #include "mutex.h"
+#include "string.h"
 #include "sleep.h"
 #include "sysCalls.h"
 #include "uartstdio.h"
@@ -18,15 +19,38 @@ static void mutPasser(void* initialDelay){
     }
 }
 
+static void mutPasserWait(void* maxWait){
+    sleepMS(500);
+    int pid = getPid();
+    struct SleepRequest req = {
+        .mSec = (uint32_t)maxWait
+    };
+    while(1){
+        int retVal = lockMutexTimeout(&mut, &req);
+        if (retVal != 0){
+            UARTprintf("Oops, the retval was not 0. ERRNO: %d (%s)\n", retVal, strerror(retVal));
+        } else{
+            UARTprintf("My pid is %d and I have a mutex!\n", pid);
+            sleepS(1);
+            unlockMutex(&mut);
+        }
+        sleepMS(1000);
+    }
+}
+
 int mainProcessLocker(void){
+    (void)(mutPasserWait);
+    (void)(mutPasser);
     int retVal = initMutex(&mut);
+    lockMutex(&mut);
     if (retVal !=0){
         errno = retVal;
         return -1;
     }
-    if (createChildProcess(128, "MutPasser1", mutPasser, (void*)1000) == -1) return -1;
-    if (createChildProcess(128, "MutPasser2", mutPasser, (void*)1500) == -1) return -1;
-    if (createChildProcess(128, "MutPasser3", mutPasser, (void*)2500) == -1) return -1;
+    if (createChildProcess(128, "MutPasser1", mutPasser, (void*)1500) == -1) return -1;
+    if (createChildProcess(128, "MutPasser2", mutPasser, (void*)2000) == -1) return -1;
+    if (createChildProcess(128, "MutPasser3", mutPasser, (void*)3000) == -1) return -1;
+    if (createChildProcess(256, "MutPasserW", mutPasserWait, (void*)500) == -1) return -1;
     return 0;
 
 }

d54b636eba49840dee869d26d1f7707ec6837cd9
Jacko Dirks <jacko.dirks@gmail.com>
2016-06-20 23:33:55 +0200

    Reworks LedsDance for new semaphore implemenation

--- project/proj/user/src/ledsDance.c
+++ project/proj/user/src/ledsDance.c
@@ -1,13 +1,13 @@
-#include "hw_types.h" //Contains the types
-#include "sysctl.h"     //Contains defines for PLL regs
-#include "rom_map.h" //Call functions directly from the ROM if available
-#include "rom.h" //Declare ROM addresses for rom funcs
-#include "hw_memmap.h" //address of GPIO etc
-#include "gpio.h" //Address of GPIO stuff
-#include "uart.h" //UART addresses and stuff
-#include "uartstdio.h" //UART printf n stuff
-#include "hw_ints.h" //Memory addresses of hw interrupts
-#include "interrupt.h"
+#include <hw_types.h> //Contains the types
+#include <sysctl.h>     //Contains defines for PLL regs
+#include <rom_map.h> //Call functions directly from the ROM if available
+#include <rom.h> //Declare ROM addresses for rom funcs
+#include <hw_memmap.h> //address of GPIO etc
+#include <gpio.h> //Address of GPIO stuff
+#include <uart.h> //UART addresses and stuff
+#include <uartstdio.h> //UART printf n stuff
+#include <hw_ints.h> //Memory addresses of hw interrupts
+#include <interrupt.h>
 
 #include "sleep.h"
 #include "process.h"
@@ -49,7 +49,7 @@ void prepareHardware(void){
 
 void gpioBInterrup(void){
     ROM_GPIOPinIntClear(GPIO_PORTB_BASE, GPIO_PIN_3);
-    increaseSemaphoreNonBlocking(&sem);
+    semaphorePost(&sem);
 }
 
 //void button2Interrupt(void){
@@ -66,13 +66,13 @@ void ledsFlicker(void){
     while(1){
         UARTprintf("Hoi\r\n");
         ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-        decreaseSemaphoreBlocking(&sem);
+        semaphoreWait(&sem);
         ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_2);
-        decreaseSemaphoreBlocking(&sem);
+        semaphoreWait(&sem);
         ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
-        decreaseSemaphoreBlocking(&sem);
+        semaphoreWait(&sem);
         ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_1);
-        decreaseSemaphoreBlocking(&sem);
+        semaphoreWait(&sem);
     }
 }
 

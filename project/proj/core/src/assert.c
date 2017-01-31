fca4f5a4396aeddc06e13418da4508bc64000fe5
Jacko Dirks <jacko.dirks@gmail.com>
2017-01-31 17:37:02 +0100

    Adds newline to assert message

--- project/proj/core/src/assert.c
+++ project/proj/core/src/assert.c
@@ -3,6 +3,6 @@
 #include <stdlib.h>
 
 void __assert_func (const char* fileName, int lineNum, const char* funcName, const char* msg){
-    UARTprintf("Assertion failure: %s:%d, %s (%s)", fileName, lineNum, funcName, msg);
+    UARTprintf("Assertion failure: %s:%d, %s (%s)\n", fileName, lineNum, funcName, msg);
     abort();
 }
